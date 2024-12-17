b848898338e96b28a9d8b47abdff7729e4c972e3
Joseph <1213037975@qq.com>
2024-12-17 12:39:22 +0800

    firstsuc

--- STU-main/front/src/utils/request.ts
+++ STU-main/front/src/utils/request.ts
@@ -1,11 +1,12 @@
 import axios from "axios"
+import store from '@/store'
 const request = axios.create({
     baseURL: "http://localhost:8080"
 })
 request.interceptors.request.use(
     (config) => {
       // 从 localStorage 获取 token
-      const token = localStorage.getItem('jwt');
+      const token = store.state.jwt
       if (token) {
         config.headers['token'] = token;
       }
